71c2332ae240c9156683de7589c57b9cbd44f50b
Shanaka Prince <shanakaprince@gmail.com>
2025-04-14 11:38:33 +0530

    first commit

---
+++ next-news-site/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Case1 } from "@/components/Case";
+import HeroSection from "../components/HeroSection";
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <HeroSection />
+      <Case1></Case1>
+    </div>
+  );
+}
